d12343874792d058b7bd10a9ca7f6d48b0078bf4
ChuTantian <CHUT0005@e.ntu.edu.sg>
2024-11-18 08:46:28 +0000

    1

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "PUT"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = request.form.get("q")
+    return(render_template("main.html"))
 
 if __name__ == "__main__":
     app.run()
