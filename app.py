c0c43e434f264b03496957f279063c61251a0630
ChuTantian <CHUT0005@e.ntu.edu.sg>
2024-12-05 05:41:05 +0000

    1

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 #
-#syn code soaces is very high risk, sometimes fall
+from flask import Flask
+from flask import render_template, request
 
-from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "PUT"])
+@app.route('/', methods=['GET', 'POST']) # front end to back end: post
 def index():
-    return render_template("index.html")
+    return(render_template('index.html'))
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/main', methods=['GET', 'POST']) # front end to back end: post
+def main():
+    name = request.form.get('q')
+    return(render_template('main.html'))
+
+if __name__ == '__main__':
+    app.run() # app.run(port=1234)
